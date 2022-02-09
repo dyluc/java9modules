9642863eab09ce2ee58153caac7f48da6da97ce4
dyluc <dylan@thenullproject.com>
2022-02-09 18:55:03 +0000

    initial commit

---
+++ src/com.example.module/com/example/module/Main.java
@@ -0,0 +1,11 @@
+package com.example.module;
+
+import java.util.logging.Logger;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info("hey, from the logger!");
+    }
+}
---
+++ src/com.example.module/module-info.java
@@ -0,0 +1,5 @@
+module com.example.module {
+    requires java.base;
+    requires java.logging;
+    exports com.example.module;
+}
